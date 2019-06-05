0b8928360586459cadb04deeb4e2e575f925cc73
Jacob <41162160+JacobPox@users.noreply.github.com>
2019-06-05 16:08:56 -0400

    Add ray casting

--- tinytracer.c
+++ tinytracer.c
@@ -51,11 +51,19 @@ bool ray_intersect(const float origin[], const float dir[], float * t0, Sphere s
     return true;
 }
 
-int cast_ray(const float origin[], const float dir[], const Sphere s) {
-    
+int cast_ray(const float origin[], const float dir[], const Sphere s, unsigned char colorArr[]) {
+    float sphere_dist = 200000;
+    float * t0 = &sphere_dist;
+    if (!ray_intersect(origin, dir, t0, s)) {
+        colorArr[0], colorArr[1], colorArr[2] = .2, .7, .8; //background color
+    } else {
+        colorArr[0], colorArr[1], colorArr[2] = .4,.4,.3;
+    }
+
+    return 0;
 }
 
-int render() {
+int render(const Sphere s) {
     /*
     Creates image in a new color each step.
     */
@@ -65,12 +73,19 @@ int render() {
     FILE *fp = fopen("first.ppm", "wb"); // Write in binary mode
     (void) fprintf(fp, "P6\n%d %d\n255\n", width, height);
 
+    float fov = 100; // Field of View
+
     for (int j = 0; j < height; ++j) {
         for (int i = 0; i < width; ++i) {
+
+            float x = (2*(i+.5)/(float)width - 1)*tan(fov/2.)*width/(float)height;
+            float y = -(2*(j+.5)/(float)height - 1)*tan(fov/2.);
+
+            float dir[] = {x,y,-1};
+
             static unsigned char color[3];
-            color[0] = i % 256; // red
-            color[1] = j % 256; //green
-            color[2] = (i * j) % 256; //blue
+            const float origin[] = {0,0,0};
+            cast_ray(origin, dir, s, color);
             (void) fwrite(color, 1, 3, fp);
         }
     }
@@ -79,7 +94,9 @@ int render() {
 }
 
 int main(void) {
-    render();
+    Sphere s = {{1,1,1}, 1};
+
+    render(s);
     printf("Run success!\n");
     return 0;
 }
